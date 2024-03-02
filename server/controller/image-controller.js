276f14402d3a9d12a6a525a44ab81046fd2f8dc5
Pooja <90396302+pooja31-05@users.noreply.github.com>
2024-03-02 10:00:31 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -12,7 +12,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}`});
+        response.status(200).json({ path: `https://filesharing-r0ip.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
